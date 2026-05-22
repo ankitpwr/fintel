9f18a89e4ce9aaf1edf776563f129f836f59f738
ankit pwr <ankitpam321@gmail.com>
2026-05-23 00:26:44 +0530

    feat: add types for balancesheet and cashflow

--- backend/types/agent.types.ts
+++ backend/types/agent.types.ts
@@ -42,3 +42,49 @@ export interface ShareHoldingInfo {
   promoterHoldingPercentage: number;
   publicHoldingPercentage: number;
 }
+export interface BalanceSheet {
+  totalAssets: number;
+  totalDebt: number;
+  currentDebt?: number;
+  longTermDebt: number;
+  netDebt: number;
+  cashAndEquivalents: number;
+  shareholdersEquity: number;
+  currentAssets: number;
+  currentLiabilities: number;
+  workingCapital: number;
+  inventory: number;
+  accountsReceivable: number;
+  accountsPayable?: number;
+  goodwillAndIntangiblesAsssets: number;
+  netPPE: number;
+  periodType: string;
+  date: string;
+}
+export interface CashFlow {
+  operatingCashFlow: number;
+  freeCashFlow: number;
+  capitalExpenditure: number;
+  investingCashFlow: number;
+  financingCashFlow: number;
+  depreciation: number;
+  changeInWorkingCapital?: number;
+  cashDividendsPaid?: number;
+  endCashPosition: number;
+  periodType: string;
+  date: string;
+}
+export interface IncomeStatement {
+  totalRevenue: number;
+  grossProfit: number;
+  operatingIncome: number;
+  ebit: number;
+  ebitda: number;
+  pretaxIncome: number;
+  netIncome: number;
+  interestExpense: number;
+  totalExpenses: number;
+  costOfRevenue: number;
+  periodType: string;
+  date: string;
+}
