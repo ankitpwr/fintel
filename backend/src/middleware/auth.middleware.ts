e04b671bea7be75be6020c69c2f971f341db0b4b
ankit pwr <ankitpam321@gmail.com>
2026-06-21 20:09:58 +0530

    feat: add auth middleware

---
+++ backend/src/middleware/auth.middleware.ts
@@ -0,0 +1,40 @@
+import "dotenv/config";
+import type { NextFunction, Request, Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+
+interface CustomPayload extends JwtPayload {
+  email: string;
+  id: string;
+}
+
+interface CustomRequest extends Request {
+  email: string;
+  id: string;
+}
+
+function authMiddleware(req: Request, res: Response, next: NextFunction) {
+  try {
+    const token = req.cookies.token || "";
+    if (!token || token == "") {
+      return res.status(401).json({
+        message: "Invalid user",
+        error: "Unauthorized User, please login first",
+      });
+    }
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET!) as CustomPayload;
+    if (!decode || !decode.email || !decode.id) {
+      return res.status(401).json({
+        message: "Invalid user",
+        error: "Unauthorized User, please login first",
+      });
+    }
+
+    (req as CustomRequest).email = decode.email;
+    (req as CustomRequest).id = decode.id;
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+}
